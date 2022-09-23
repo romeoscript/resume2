ad4bae7dc15a0d967e6b8cff463ca989667dea9a
romeoscript <romeobourne211@gmail.com>
2022-09-23 16:17:08 +0100

    first uploadt

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useRef, useState } from "react";
+import "./Contact.css";
+import emailjs from "@emailjs/browser";
+import { themeContext } from "../../Context";
+const Contact = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  const form = useRef();
+  const [done, setDone] = useState(false)
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_nvujpvg",
+        "template_yv34mfe",
+        form.current,
+        "AwdcZlkLqsFFrC05Z"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+          form.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="contact-form" id="contact">
+      {/* left side copy and paste from work section */}
+      <div className="w-left">
+        <div className="awesome">
+          {/* darkMode */}
+          <span style={{color: darkMode?'white': ''}}>Get in Touch</span>
+          <span>Contact me</span>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+      </div>
+      {/* right side form */}
+      <div className="c-right">
+   
+      <br />
+        <form ref={form} onSubmit={sendEmail}>
+        <p>{done && "Thanks for Contacting me"}</p>
+          <input type="text" name="user_name" className="user"  placeholder="Name"/>
+          <input type="email" name="user_email" className="user" placeholder="Email"/>
+          <textarea name="message" className="user" placeholder="Message"/>
+          <input type="submit" value="Send" className="button"/>
+          
+          <div
+            className="blur c-blur1"
+            style={{ background: "var(--purple)" }}
+          ></div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
